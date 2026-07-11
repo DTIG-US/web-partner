95fc21f1ec7b975d84f4787a6fe94558b2eea057
John Aziz <john.aziz@dtig.us>
2026-07-11 18:18:23 -0500

    [Fixed] JavaScript naming collision

--- partners.component.ts
+++ partners.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-partners',
@@ -11,20 +12,8 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 })
 export class PartnersComponent {
 
-  slides = [
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/666666"}
-  ];
+  partnersData: any = (siteData as any).default;
   slideConfig = {"slidesToShow": 5, "slidesToScroll": 2, "dots": true, "autoplay": true, arrows: false};
-
 
   slickInit(e: any) {
     console.log('slick initialized');
