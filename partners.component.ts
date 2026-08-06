3c0529e1ff84fc1962688d61278cbe7861673708
John Aziz <john.aziz@dtig.us>
2026-08-06 11:37:57 -0500

    feat: redesign partner cards and update slide content

--- partners.component.ts
+++ partners.component.ts
@@ -1,34 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-partners',
-  standalone: true as boolean,
-  imports: [CommonModule, SlickCarouselModule],
+  imports: [CommonModule],
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.css'],
 })
 export class PartnersComponent {
-
   partnersData: any = (siteData as any).default;
-  slideConfig = {"slidesToShow": 5, "slidesToScroll": 2, "dots": true, "autoplay": true, arrows: false};
-
-  slickInit(e: any) {
-    console.log('slick initialized');
-  }
-
-  breakpoint(e: any) {
-    console.log('breakpoint');
-  }
-
-  afterChange(e: any) {
-    console.log('afterChange');
-  }
-
-  beforeChange(e: any) {
-    console.log('beforeChange');
-  }
-
 }
+
