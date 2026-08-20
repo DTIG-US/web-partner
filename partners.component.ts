7574ac854ad1f4880678221b78a05d27107c228d
John Aziz <john.aziz@dtig.us>
2026-08-20 09:48:12 -0500

    [Fixed] Menu issues

--- partners.component.ts
+++ partners.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CommonModule } from '@angular/common';
 import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-partners',
+  standalone: true as boolean,
+  imports: [CommonModule, SlickCarouselModule],
+  standalone: true as boolean,
+  imports: [CommonModule, SlickCarouselModule],
   imports: [CommonModule],
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.css'],
