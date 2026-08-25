945601d10f9f50acbb26f5e873566cb4b36777ed
John Aziz <john.aziz@dtig.us>
2026-08-25 15:16:36 -0500

    [Fixed] Duplicate Angular reference insertions

--- partners.component.ts
+++ partners.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { CommonModule } from '@angular/common';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { CommonModule } from '@angular/common';
 import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-partners',
   standalone: true as boolean,
   imports: [CommonModule, SlickCarouselModule],
-  standalone: true as boolean,
-  imports: [CommonModule, SlickCarouselModule],
-  imports: [CommonModule],
   templateUrl: './partners.component.html',
   styleUrls: ['./partners.component.css'],
 })
